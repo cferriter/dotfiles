5f86591e5cb5ee377e1d8b27bce997e68d7dd21b
Cian Ferriter <cianbferriter@gmail.com>
2020-11-24 22:36:36 +0000

    Improve makesymlinks.sh
    Improvements:
    * Use uppercase for script variables.
    * Wrap lines correctly.
    * Use variable names rather than hardcoded paths in comments.
    * Remove some comments and echo cmds.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-############################
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles.
-############################
+# This script creates symlinks from the home directory to any desired dotfiles
+# in $DOTFILES_DIR.
 
 # Dotfiles directory.
-dir=~/dotfiles
+DOTFILES_DIR=~/dotfiles
 # Old dotfiles backup directory.
-olddir=~/dotfiles_old
-# List of files/folders to symlink in $HOME.
-files=".vimrc .tmux.conf .tigrc .gitconfig .gdbinit .bashrc .bash_profile .bash_aliases .gitignore"
+OLD_DIR=~/dotfiles_old
+# List of dotfiles to create symlinks to in $HOME.
+files=".vimrc .tmux.conf .tigrc .gitconfig .gdbinit .bashrc .bash_profile
+.bash_aliases .gitignore"
 
-# Create dotfiles_old in $HOME.
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+echo "Creating $OLD_DIR for backup of any existing dotfiles in $HOME"
+mkdir -p $OLD_DIR
 
-# Change to the dotfiles directory.
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
+cd $DOTFILES_DIR
 
-# Move any existing dotfiles in $HOME to dotfiles_old directory.
-# Create symlinks from the $HOME to any files in the ~/dotfiles directory specified in $files.
+echo "Moving any existing dotfiles in $HOME to $OLD_DIR"
 for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/$file ~/dotfiles_old/
-	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/$file
+	mv ~/$file ~/$OLD_DIR/
+	echo "Creating symlink to $file in $HOME"
+	ln -s $DOTFILES_DIR/$file ~/$file
 done
