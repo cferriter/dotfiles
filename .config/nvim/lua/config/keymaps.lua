dadf9898be6d21bfbe4169174e5d2b782bf7fe93
Cian Ferriter <cianbferriter@gmail.com>
2024-12-19 14:05:18 +0000

    Remove duplicate Neovim shortcuts
    There are existing shortcuts for finding files and commenting lines of
    code.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- List all files to fuzzy search on.
-vim.keymap.set( 'n', '<C-P>', ':Telescope find_files<CR>')
-
 -- Delete the file currently open.
 local function confirm_and_delete_buffer()
   local confirm = vim.fn.confirm("Delete buffer and file?", "&Yes\n&No", 2)
@@ -15,16 +12,3 @@ local function confirm_and_delete_buffer()
   end
 end
 vim.keymap.set('n', '<leader>rm', confirm_and_delete_buffer)
-
--- Toggle comment.
-local operator_rhs = function()
-    return require('vim._comment').operator()
-end
-vim.keymap.set('v', "<C-F>", operator_rhs, { expr = true, desc = 'Toggle comment' })
-
-local line_rhs = function()
-    return require('vim._comment').operator() .. '_'
-end
-vim.keymap.set('n', "<C-F>", line_rhs, { expr = true, desc = 'Toggle comment line' })
-
-
