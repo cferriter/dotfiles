9cf02c67e933d93aec1a74229c039e9076dfdfae
Cian Ferriter <cianbferriter@gmail.com>
2025-03-22 21:52:59 +0000

    Add WezTerm config and create symlinks for Windows

---
+++ .wezterm.lua
@@ -0,0 +1,13 @@
+-- Pull in the WezTerm API.
+local wezterm = require 'wezterm'
+
+-- Create a convenient config variable.
+local config = wezterm.config_builder()
+
+config.font_size = 10
+config.color_scheme = 'Tokyo Night Moon'
+config.hide_tab_bar_if_only_one_tab = true
+config.default_domain = 'WSL:Ubuntu'
+
+-- Return the configuration to WezTerm.
+return config
--- create_win_symlinks.sh
+++ create_win_symlinks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-ps_command='New-Item -ItemType SymbolicLink -Path $env:APPDATA\kanata\kanata.kbd -Target \\wsl$\Ubuntu\root\dotfiles\kanata.kbd'
-powershell.exe -Command "Start-Process PowerShell -Verb RunAs -ArgumentList '$ps_command'"
+create_kanata_symlink='New-Item -ItemType SymbolicLink -Path $env:APPDATA\kanata\kanata.kbd -Target \\wsl$\Ubuntu\root\dotfiles\kanata.kbd'
+powershell.exe -Command "Start-Process PowerShell -Verb RunAs -ArgumentList '$create_kanata_symlink'"
+
+create_wezterm_symlink='New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\.wezterm.lua -Target \\wsl$\Ubuntu\root\dotfiles\.wezterm.lua'
+powershell.exe -Command "Start-Process PowerShell -Verb RunAs -ArgumentList '$create_wezterm_symlink'"
